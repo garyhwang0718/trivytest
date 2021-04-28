281656986d995f643eb84986c38c562991f1794b
rukechen <rukechen@qnap.com>
2021-04-28 14:09:46 +0800

    Update qne.mk

--- debian/qne.mk
+++ debian/qne.mk
@@ -9,3 +9,8 @@ SUBSTVARS := \
 override_dh_gencontrol:
 	dh_gencontrol -- $(SUBSTVARS)
 
+override_dh_installdeb:
+	dh_installdeb
+	-for pkg in $$(dh_listpackages); do \
+		sed -i -e 's/__DEB_VERSION__/$(DEB_VERSION)/' debian/$$pkg/DEBIAN/* 2>/dev/null || true; \
+		done
