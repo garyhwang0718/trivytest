6b91c447fd1b51c6a62f7a21dacf4de47e7feaf0
rukechen <rukechen@qnap.com>
2021-06-16 14:50:34 +0800

    [QNEQUNDR-667] As discussed, revise the response format.

--- health/healthcheck.sh
+++ health/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kapacitor=-1
-influxdb=-1
+kapacitor=1
+influxdb=1
 curl -s 127.0.0.1:9092/kapacitor/v1/ping
 if [ $? -eq 0 ] ;then
     kapacitor=0
@@ -9,4 +9,5 @@ curl -s --unix-socket /var/run/influxdb/influxdb.sock -G 'http://localhost/ping?
 if [ $? -eq 0 ] ;then
     influxdb=0
 fi
-echo "{\"influxdb\": $influxdb, \"kapacitor\": $kapacitor}"
+echo "{\"error_code\":0,\"status\":{\"influxdb\":$influxdb,\"kapacitor\":$kapacitor}}"
+
